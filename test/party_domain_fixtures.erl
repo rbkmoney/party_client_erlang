922ddf4d9dba808418e9e1080adf75fe20e6be04
Toporkov Igor <i.toporkov@rbkmoney.com>
2020-07-16 11:04:13 +0300

    DC-122: Remove risk coverage from terminal (#14)
    * Upgrade damsel
    
    * Update composed services
    
    * Remove risk_coverage
    
    * terms -> terms_legacy

--- test/party_domain_fixtures.erl
+++ test/party_domain_fixtures.erl
@@ -358,8 +358,7 @@ construct_domain_fixture() ->
             data = #domain_Terminal{
                 name = <<"Brominal 1">>,
                 description = <<"Brominal 1">>,
-                risk_coverage = high,
-                terms = #domain_PaymentsProvisionTerms{
+                terms_legacy = #domain_PaymentsProvisionTerms{
                     payment_methods = {value, ?ordset([
                         ?pmt(bank_card, visa)
                     ])}
